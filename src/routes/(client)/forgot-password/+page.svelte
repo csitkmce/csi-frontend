45c67d156d93a673054efbba8ccbd588b403f295
nearHaan <farhaan.nizam675@gmail.com>
2025-10-29 18:46:06 +0530

    add forgot password page

---
+++ src/routes/(client)/forgot-password/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	let errorText = $state('');
+	let errorField = $state('');
+</script>
+
+<div class="flex h-screen w-full flex-col items-center bg-white p-10 text-black">
+	<h1 class="text-3xl text-[#222222]">Forgot Password</h1>
+	<div class="mt-10 min-w-sm border-1 border-black p-3 shadow-[4px_4px_0_0_black]">
+		<p class="text-sm">Enter your registered email address</p>
+		<input
+			name="email"
+			class="mt-2 h-8 w-full rounded-xs border-1 border-black p-2 {errorField === 'email'
+				? 'border-red-500'
+				: ''}"
+			type="email"
+		/>
+		<button
+			type="submit"
+			class="mt-4 w-full cursor-pointer border-1 border-black bg-[#D9D9D9] px-6 py-3 text-sm ease-in-out hover:bg-black hover:text-white"
+		>
+			Send me reset link
+		</button>
+	</div>
+</div>
