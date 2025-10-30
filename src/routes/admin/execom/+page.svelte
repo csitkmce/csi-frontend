adc993ff42426cc4b9ab7920f09ce83505d9c79b
nearHaan <farhaan.nizam675@gmail.com>
2025-10-30 15:03:38 +0530

    ui update

--- src/routes/admin/execom/+page.svelte
+++ src/routes/admin/execom/+page.svelte
@@ -2,11 +2,11 @@
 	import type { UserDataRow } from '$lib/types';
 	import AdminTitleBtn from '$lib/components/admin-title-btn.svelte';
 	import AdminSearch from '$lib/components/admin-search.svelte';
-	import ActionsPane from '$lib/components/admin-actions.svelte'
+	import ActionsPane from '$lib/components/admin-actions.svelte';
 
 	let dm_choices = ['Delete'];
 	let searchText: string = $state('');
-    let selected : string = dm_choices[0];
+	let selected: string = dm_choices[0];
 	let users: UserDataRow[] = [
 		{
 			status: false,
@@ -43,15 +43,15 @@
 		}
 	];
 
-	function onAddClick(){
+	function onAddClick() {
 		//
 	}
 
-	function onSearch(){
+	function onSearch() {
 		//
 	}
 
-	function onActionGo(){
+	function onActionGo() {
 		//
 	}
 </script>
@@ -59,12 +59,16 @@
 <div
 	class="flex min-h-screen w-full max-w-7xl flex-col items-center border-x-1 border-black bg-[#222222] p-4 text-white"
 >
-	<AdminTitleBtn title="Manage Execom Members" buttonText="Add Member" onPrimaryClick={onAddClick}/>
+	<AdminTitleBtn
+		title="Manage Execom Members"
+		buttonText="Add Member"
+		onPrimaryClick={onAddClick}
+	/>
 	<!-- Search bar -->
-	<AdminSearch searchText={searchText} onSearch={onSearch}/>
+	<AdminSearch {searchText} {onSearch} />
 
 	<!-- Actions pane -->
-	<ActionsPane dm_choices={dm_choices} selected={selected} onGo={onActionGo}/>
+	<ActionsPane {dm_choices} {selected} onGo={onActionGo} />
 
 	<!-- Main body -->
 	<div class="mt-4 w-full overflow-auto border-1 border-black text-[#eeeeee]">
@@ -72,28 +76,28 @@
 			<thead class="bg-black">
 				<tr>
 					<th class="min-w-10 border"><input type="checkbox" /></th>
-					<th class="test-sm min-w-20 border p-2">UserID</th>
-					<th class="test-sm min-w-50 border p-2 text-left">Name</th>
-					<th class="test-sm min-w-80 border p-2 text-left">Email</th>
-					<th class="test-sm min-w-40 border p-2 text-left">Phone number</th>
-					<th class="test-sm min-w-50 border p-2 text-left">Deapartment</th>
-					<th class="test-sm min-w-30 border p-2">Batch</th>
-					<th class="test-sm min-w-30 border p-2">Year</th>
-					<th class="test-sm min-w-30 border p-2">Role</th>
+					<th class="min-w-20 border p-2 text-sm">UserID</th>
+					<th class="min-w-50 border p-2 text-left text-sm">Name</th>
+					<th class="min-w-80 border p-2 text-left text-sm">Email</th>
+					<th class="min-w-40 border p-2 text-left text-sm">Phone number</th>
+					<th class="min-w-50 border p-2 text-left text-sm">Deapartment</th>
+					<th class="min-w-30 border p-2 text-sm">Batch</th>
+					<th class="min-w-30 border p-2 text-sm">Year</th>
+					<th class="min-w-30 border p-2 text-sm">Role</th>
 				</tr>
 			</thead>
 			<tbody>
 				{#each users as user (user.id)}
 					<tr class="odd:bg-[#353535]">
 						<th class="min-w-10 border"><input type="checkbox" checked={user.status} /></th>
-						<th class="test-sm border p-2">{user.id}</th>
-						<th class="test-sm border p-2 text-left">{user.name}</th>
-						<th class="test-sm border p-2 text-left">{user.email}</th>
-						<th class="test-sm border p-2 text-left">{user.phone_number}</th>
-						<th class="test-sm border p-2 text-left">{user.department}</th>
-						<th class="test-sm border p-2">{user.batch}</th>
-						<th class="test-sm border p-2">{user.year}</th>
-						<th class="test-sm border p-2">{user.role}</th>
+						<th class="border p-2 text-sm">{user.id}</th>
+						<th class="border p-2 text-left text-sm">{user.name}</th>
+						<th class="border p-2 text-left text-sm">{user.email}</th>
+						<th class="border p-2 text-left text-sm">{user.phone_number}</th>
+						<th class="border p-2 text-left text-sm">{user.department}</th>
+						<th class="border p-2 text-sm">{user.batch}</th>
+						<th class="border p-2 text-sm">{user.year}</th>
+						<th class="border p-2 text-sm">{user.role}</th>
 					</tr>
 				{/each}
 			</tbody>
