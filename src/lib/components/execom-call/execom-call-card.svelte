89e1b8fab0719d5c3cde46ff852f07f4a9dc6039
nearHaan <farhaan.nizam675@gmail.com>
2026-05-16 12:43:28 +0530

    fix typo error

--- src/lib/components/execom-call/execom-call-card.svelte
+++ src/lib/components/execom-call/execom-call-card.svelte
@@ -9,7 +9,7 @@
 <div class="my-4 flex flex-col items-start bg-blue-900 px-4 py-2 font-sans text-sm text-blue-300">
 	<h2 class="text-lg font-bold">Execom Call (2026-27)</h2>
 	<p class="mt-1 text-justify">
-		CSI TKMCE prudly invites enthusiastic, motivated, and visionary students to be part of the new
+		CSI TKMCE proudly invites enthusiastic, motivated, and visionary students to be part of the new
 		Executive Committie (Execom) for the upcoming term
 	</p>
 	<button
